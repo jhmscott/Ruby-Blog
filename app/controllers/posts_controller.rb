1503afd160977f0223f65fda1cae9a2f15ab5ebb
Justin Scott <justinhmscott@gmail.com>
2020-02-05 12:33:24 -0800

    Added models

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+  def index
+    @content = "This is sample text for a post";  
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+end
